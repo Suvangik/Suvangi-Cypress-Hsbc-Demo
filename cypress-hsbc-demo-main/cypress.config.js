f7211c8c1a43e69f114998ef0aa6d2f4452b7682
Suvangik <suvangikumari1999@gmail.com>
2026-04-28 00:26:20 +0530

    Fix casing of baseUrl in cypress config

--- cypress-hsbc-demo-main/cypress.config.js
+++ cypress-hsbc-demo-main/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://www.hsbc.co.in/",
+    baseurl: "https://www.hsbc.co.in/",
     viewportWidth: 1280,
     viewportHeight: 720,
     chromeWebSecurity: false,
